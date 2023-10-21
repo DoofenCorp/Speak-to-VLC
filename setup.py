3613b64b5295524ce91d63e312553e533a70c24a
DoofenCorp <48542507+DoofenCorp@users.noreply.github.com>
2023-10-22 00:52:10 +0530

    Implemented preference for noise adjustment

--- setup.py
+++ setup.py
@@ -11,14 +11,21 @@ try:
     port = int(input("Provide the port (default 4212): "))
 except ValueError:
     port = 4212
+noise = input("Adjust microphone input for noise (set to no by default) (y/n): ")
+
+noise  = True if "y" in noise else False
+
+
 key, token = get_password()
 configuration['host'] = host
 configuration['port'] = port
+configuration['noise'] = noise
 configuration['key'] = str(key.decode('utf-8'))
 configuration['token'] = str(token.decode('utf-8'))
 
 with open("config.json", "w") as config:
-    print("Setting host: {0}".format(host))
+    print("\nSetting host: {0}".format(host))
     print("Setting port: {0}".format(port))
+    print("Adjust for noise: {0}".format(noise))
     json.dump(configuration, config)
     print("Written config.json to", getcwd())
